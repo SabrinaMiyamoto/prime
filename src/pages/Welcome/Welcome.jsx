03a2bac4a4a30995d1f2344b4422d351b22cbe5b
Eder Domenici <domenicieder@gmail.com>
2024-03-14 20:01:12 -0300

    pushing

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import Prime from '../../assets/img/prime.png';
+import Prime from '../../assets/img/NOVA logo Prime.svg';
 import './Welcome.css';
 import { Link } from 'react-scroll';
 
@@ -7,15 +7,18 @@ const Welcome = () => {
     <div>
         <section className="container">
             <div id="container">
-            <img src={Prime} alt="um triângulo vazado, com a escrita prime gráfica e Web" />
-            <div className="vertical-line"></div>
-            <div className="text">
-                <h1>Seja Bem-vindo!</h1>
-                <p>Inovação, qualidade e excêlencia:</p>
-                <p>Entre, fique a vontade, a casa é sua!</p>
+              <div>
+                <img src={Prime} alt="um triângulo vazado, com a escrita prime gráfica e Web" />
+                <h1 className="title-logo">PRIME</h1>
+                <h2 className='subtitle-logo'>Gráfica & Web</h2>
+              </div>
+              <div className="vertical-line"></div>
+              <div className="text">
+                  <h1>Seja Bem-vindo!</h1>
+                  <p>Inovação, qualidade e excêlencia:</p>
+                  <p>Entre, fique a vontade, a casa é sua!</p>
+              </div>
             </div>
-            </div>
-            <button className="btn"><Link to='contact' smooth={true}>Entrar em Contato</Link></button>
         </section>
     </div>
   )
